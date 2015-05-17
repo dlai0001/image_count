160c15791c7709c8220edf8f4692917e8f7eac98
David Lai <me@dlai0001.com>
2015-05-16 17:53:53 -0700

    add argparse into image_count script

--- image_count.py
+++ image_count.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 
 # TODO: implement this
-print "nothing to see here yet."
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-k','--key', help='Api Key', required=True)
+args = parser.parse_args()
+
+
+# TODO
+print("api key used: " + args.key)
+print("Nothing to see here, come back when this program is done.")
