3bae24b634ebed75363e0960deca11fa1af5fb77
David Lai <me@dlai0001.com>
2015-05-17 01:20:10 -0700

    ditch the fancy dependency injection - will use a simpler optional constructor arg injection instead.
    update read me.

--- shovel/test.py
+++ shovel/test.py
@@ -1,9 +1,11 @@
 import os
 from shovel import task
 
+COVERAGE_SETTINGS = " --with-coverage --cover-branches --cover-min-percentage=95 --cover-package=image_count "
+
 @task
 def unit():
-    os.system("nosetests src/")
+    os.system("nosetests src/" + COVERAGE_SETTINGS)
 
 
 @task
@@ -13,4 +15,4 @@ def integration():
 
 @task
 def path(path):
-	os.system("nosetests " + path + "/" )
+	os.system("nosetests " + path + "/" + COVERAGE_SETTINGS)
--- src/image_count/__init__.py
+++ src/image_count/__init__.py
@@ -3,5 +3,4 @@ Depedencies used by image_count module.
 
 Dependencies are defined as lambas
 """
-
-_dependencies = None
+pass
